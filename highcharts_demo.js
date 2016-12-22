89a9ddfa9e8c0fa27ae744ee2c74fc4268453e7c
Emma Nelson <eknelson17@gmail.com>
2016-12-22 12:29:01 -0600

    highcharts: Add stacked column graph.

--- highcharts_demo.js
+++ highcharts_demo.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
       data: [90, 88, 89, 92]
     }]
   });
-
+  
   var columnChart = Highcharts.chart('column-chart', {
     chart: {
       type: 'column'
@@ -45,6 +45,61 @@ $(document).ready(function () {
       data: [100, 70, 84, 62, 79, 87]
     }]
   });
+
+  var stackedColumnChart = Highcharts.chart('stacked-column-chart', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Change in Grades over the Last Week'
+        },
+        xAxis: {
+            categories: ['Luke Skywalker', 'Kylo Ren', 'Mace Windu', 'Obi Wan Kenobi', 'Anakin Skywalker', 'Qui-Gon Ginn']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Grade (%)'
+            },
+            reversedStacks: false,
+        },
+        legend: {
+            align: 'right',
+            x: -30,
+            verticalAlign: 'top',
+            y: 25,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+        },
+        tooltip: {
+            headerFormat: '<b>{point.x}</b><br/>',
+            pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+        },
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+                tooltip: {
+                  valueSuffix: '%'
+                }
+            }
+        },
+        series: [{
+          name: 'Grade',
+          data: [70, 50, 100, 80, 15, 90],
+          color: '#666'
+        },{
+          name: 'Increase',
+          data: [10, 0, 0, 5, 0, 0],
+          color: 'green'
+        },{
+          name: 'Decrease',
+          data: [0, 10, 0, 0, 5, 0],
+          color: 'red'
+        }]
+    });
 
   var barChart = Highcharts.chart('bar-chart', {
     chart: {
